7386d959d9ee362375aa21c3fe915b40eabb2dba
Techydalgo <suporte.techydalgo@gmail.com>
2026-03-03 22:13:30 -0300

    primeiro commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyADWdtpqdQjgcKtL09QQoB0Pv-PYCWm6Ow",
+  apiKey: "AIzaSyADWdtxpdQjgckTl09QqoBOPv-PYCWm6Ow",
   authDomain: "techydalgo.firebaseapp.com",
   projectId: "techydalgo",
   storageBucket: "techydalgo.firebasestorage.app",
   messagingSenderId: "192638237601",
-  appId: "1:192638237601:web:15e766b7ea14e35001a9e0"
+  appId: "1:192638237601:web:15e766b7ea14e35001a9e0",
+  measurementId: "G-YEXN4QZV6T"
 };
 
 const app = initializeApp(firebaseConfig);
